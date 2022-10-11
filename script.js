7cb366e3a992b131b232fcd093aea2b4dcab260d
Zevitorb <benvenuttijosevitor@gmail.com>
2022-10-11 17:24:33 -0300

    Entrega finalizada
    Adicionado index html, css e js. Entrega finalizada

---
+++ script.js
@@ -0,0 +1,66 @@
+// Exercício 1
+// let valorLitro = 6.80;
+// let valorDaCompra = parseFloat(prompt("Informe valor em reais que deseja abastecer"));
+// let qtdLitros = valorDaCompra / valorLitro;
+// alert(`O Cliente abasteceu: ${qtdLitros.toFixed(2)} Litros`);
+
+//Exercico 2
+// let tempCelsius = parseInt(prompt("Informe quantos graus Celsius"));
+// let tempFahre = (9*tempCelsius+160)/5;
+// alert(`O valor de ${tempCelsius}°C em Fahrenheit é ${tempFahre.toFixed(0)}°F`)
+
+//Exercício 3
+// let tempFahre = parseInt(prompt("Informe quantos graus Fahrenheit"));
+// let tempCelsius = ((tempFahre-32)*5)/9;
+// alert(`O valor de ${tempFahre}°F em Celsius é ${tempCelsius.toFixed(0)}°C`);
+
+//Exercício 4
+// let comprimento = parseFloat(prompt("Informe o comprimento:"));
+// let largura = parseFloat(prompt("Informe a largura:"));
+// let altura = parseFloat(prompt("Informe a altura:"));
+// alert(`O volume é de: ${comprimento*largura*altura}m³`)
+
+//Exercício 5
+// let valorNumerico = parseInt(prompt("Informe um valor numérico inteiro:"));
+// alert(`O valor ${valorNumerico} ao quadrado é ${valorNumerico*valorNumerico}`);
+
+//Exercício 6
+// let nota1 = parseFloat(prompt("Insira o valor da nota 1:"));
+// let nota2 = parseFloat(prompt("Insira o valor da nota 2:"));
+// let nota3 = parseFloat(prompt("Insira o valor da nota 3:"));
+// let nota4 = parseFloat(prompt("Insira o valor da nota 4:"));
+// let nota5 = parseFloat(prompt("Insira o valor da nota 5:"));
+// alert(`A soma das notas informadas é: ${nota1+nota2+nota3+nota4+nota5} e a média das notas é: ${(nota1+nota2+nota3+nota4+nota5)/5}`)
+
+//Exercício 7
+// let valor1 = parseInt(prompt("Insira um número inteiro positivo:"));
+// let valor2 = parseInt(prompt("Insira outro número inteiro positivo:"));
+// alert(`O resto da divisão de ${valor1} por ${valor2} é: ${valor1%valor2}`)
+
+//Exercício 8
+// let texto = prompt("Insira o texto desejado.");
+// let textoLength = texto.length;
+// alert(`Este texto contém ${textoLength} caracteres.`);
+
+//Exercício 9
+// let texto = prompt("Insira o texto desejado.");
+// let primeiraLetra = texto[0];
+// let ultimaLetra = texto[texto.length-1];
+// alert(`A primeira letra deste texto é ${primeiraLetra.toUpperCase()} e a última é ${ultimaLetra.toLowerCase()}`);
+
+//Exercício 10
+// let nome = prompt("Insiria seu nome");
+// let sobrenome = prompt("Insira seu sobrenome");
+// let idade = prompt("Insira sua idade");
+// let setor = prompt("Insira o setor em que você trabalha");
+// alert(`Nome: ${nome.substring(0,1).toUpperCase()}${nome.substring(1, 255)} ${sobrenome.substring(0,1).toUpperCase()}${sobrenome.substring(1, 255)} - Idade: ${idade} - Setor: ${setor.substring(0,1).toUpperCase()}${setor.substring(1, 255)}`);
+
+
+
+
+
+
+
+
+
+
